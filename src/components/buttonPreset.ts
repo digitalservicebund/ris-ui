a150c530bb2487573e4cceef05711fd2016f5e1e
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-07-30 12:22:39 +0200

    feat(button): consolidate focus styles and optimize shared styles for button component
    RISDEV-4376

--- src/components/buttonPreset.ts
+++ src/components/buttonPreset.ts
@@ -10,42 +10,44 @@ export default {
             'max-w-full',
             'rounded-none',
             'box-border',
+            'border',
+            "border-2",
+            !context.disabled && 'focus:outline-none focus:ring-4 focus:ring-offset-4',
 
             // Icon position adjustments
-            instance.hasIcon && props.iconPos === 'left' && props.label ? 'pl-20 pr-24 gap-8' : '',
-            instance.hasIcon && props.iconPos === 'right' && props.label ? 'pl-24 pr-20 gap-8' : '',
+            instance.hasIcon && props.label ? (
+                props.iconPos === 'left' ? 'pl-20 pr-24 gap-8' :
+                    props.iconPos === 'right' ? 'pl-24 pr-20 gap-8' : ''
+            ) : '',
             instance.hasIcon && !props.label ? 'p-0 gap-0' : '',
 
             // Size adjustments
-            props.label && props.size === 'large' ? 'py-[1.21875rem] px-24 text-[1.125rem] leading-[1.3888] min-h-64' : '',
-            !props.label && props.size === 'large' ? 'w-64 h-64' : '',
-            !props.label && props.size !== 'large' ? 'w-48 h-48' : '',
-            props.label && props.size !== 'large' ? 'py-[0.5625rem] px-16 text-base min-h-48' : '',
+            props.size === 'large' ? (
+                props.label ? 'py-[1.21875rem] px-24 text-[1.125rem] leading-[1.3888] min-h-64' : 'w-64 h-64'
+            ) : (
+                props.label ? 'py-[0.5625rem] px-16 text-base min-h-48' : 'w-48 h-48'
+            ),
 
+            // Primary severity styles
             props.severity === 'primary' && `
-                bg-blue-800 border border-2 border-transparent
-                ${context.disabled ? 'bg-gray-400 cursor-not-allowed text-gray-600' : ''}
-                ${!context.disabled ? 'hover:bg-blue-700 text-white' : ''}
+                bg-blue-800 border-transparent text-white
+                ${context.disabled ? 'bg-gray-400 cursor-not-allowed text-gray-600' : 'hover:bg-blue-700'}
                 active:bg-blue-500 active:text-blue-800
                 focus:outline-none focus:ring-blue-800 focus:ring-4 focus:ring-offset-4
             `,
 
             // Secondary severity styles
             props.severity === 'secondary' && `
-                bg-white text-blue-800 border border-2 border-solid border-blue-800
-                ${!context.disabled && !context.active ? 'hover:bg-gray-200' : ''}
-                active:bg-white active:text-blue-800 active:border-transparent
+                bg-white text-blue-800 border-solid border-blue-800
+                ${context.disabled ? 'bg-white cursor-not-allowed text-blue-800 text-opacity-50 border-blue-800 border-opacity-50' : 'hover:bg-gray-200 active:bg-white active:text-blue-800 active:border-transparent'}
                 focus:outline-none focus:ring-blue-800 focus:ring-4 focus:ring-offset-2 focus:bg-gray-200
-                 ${context.disabled ? 'bg-white cursor-not-allowed text-blue-800 text-opacity-50 border-blue-800 border-opacity-50' : ''}
             `,
 
-            // Base ghost styles
+            // Ghost severity styles
             props.severity === 'ghost' && `
-                bg-transparent text-blue-800 border-2 border-transparent
-                ${!context.disabled && !context.active ? 'hover:bg-white hover:border-gray-500 hover:border-2 hover:border-solid' : ''}
-                active:bg-white active:border-transparent
+                bg-transparent text-blue-800 border-transparent
+                ${context.disabled ? 'cursor-not-allowed text-gray-800 text-opacity-50' : 'hover:bg-white hover:border-gray-500 hover:border-2 hover:border-solid active:bg-white active:border-transparent'}
                 focus:outline-none focus:ring-blue-800 focus:ring-4 focus:ring-offset-4 focus:bg-white focus:border-gray-500 focus:border-2 focus:border-solid
-               ${context.disabled ? 'cursor-not-allowed text-gray-800 text-opacity-50' : ''}
             `,
 
         ]
