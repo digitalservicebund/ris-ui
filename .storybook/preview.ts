2fb5dcde419bc8f16aff36daefaf079e2cb59907
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-07-31 23:23:15 +0200

    Remove icon global style from preview.ts
    RISDEV-4376

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,16 +4,6 @@ import customPresets from "../src/components/customPresets";
 import 'primeicons/primeicons.css';
 import "../src/style.css";
 
-const globalStyles = `
-  .pi {
-    font-size: 20px; 
-  }
-`;
-
-const style = document.createElement('style');
-style.innerHTML = globalStyles;
-document.head.appendChild(style);
-
 setup((app) => {
   app.use(PrimeVue, {
     pt: customPresets,
