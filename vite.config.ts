00a2534ea9e2578f8e0ee0cfeaa41041bea003c6
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-07-31 23:22:29 +0200

    Configure unplugin Icons in viteconfig
    RISDEV-4376

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+    icons({
+      autoInstall: true,
+      scale: 1.3333, // ~24px at the current default font size of 18px
+      compiler: "vue3",
+    }),
+  ],
 })
