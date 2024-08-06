b830cabf66954b6294f700c305d5837decc39c73
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-08-07 00:45:40 +0200

    Import textinputpreset into main customPresets.ts
    RISDEV-4377

--- src/components/customPresets.ts
+++ src/components/customPresets.ts
@@ -1,7 +1,9 @@
 import buttonPreset from './buttonPreset';
+import inputTextPreset from "./inputTextPreset";
 
 const customPresets = {
     button: buttonPreset,
+    inputText: inputTextPreset
 };
 
 export default customPresets;
