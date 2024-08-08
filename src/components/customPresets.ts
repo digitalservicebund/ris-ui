f282498bb5260163e68a5107870060da0dc57cb4
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-08-08 13:22:10 +0200

    Adds New presets
    RISDEV-4377

--- src/components/customPresets.ts
+++ src/components/customPresets.ts
@@ -1,9 +1,13 @@
 import buttonPreset from './buttonPreset';
 import inputTextPreset from "./inputTextPreset";
+import inputGroupPreset from "./inputGroupPreset";
+import inputGroupAddonPreset from "./inputGroupAddonPreset";
 
 const customPresets = {
     button: buttonPreset,
-    inputText: inputTextPreset
+    inputText: inputTextPreset,
+    inputGroup: inputGroupPreset,
+    inputGroupAddon: inputGroupAddonPreset,
 };
 
 export default customPresets;
