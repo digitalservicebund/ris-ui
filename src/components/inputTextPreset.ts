9cf39cc83a5e1ee598e3bb1168cdde664fe34870
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-08-08 12:02:57 +0200

    Clean up of input text preset
    RISDEV-4377

--- src/components/inputTextPreset.ts
+++ src/components/inputTextPreset.ts
@@ -2,25 +2,18 @@ interface InputTextProps {
     value?: string;
     size?: 'small' | 'large';
     placeholder?: string;
-    readOnly?: boolean;
-    error?: boolean;
-    variant?: 'filled';
     invalid?: boolean;
-
 }
 
 interface InputTextContext {
     disabled: boolean;
-    readOnly: boolean;
 }
 
 export default {
     root: ({ props, context }: { props: InputTextProps; context: InputTextContext }) => {
-
         let sizeStyling = 'h-64 px-24';
         let stateStyling = '';
         let errorStyling = '';
-        let successStyling = '';
 
         if (props.size === 'small') {
             sizeStyling = 'h-48 px-16 py-11';
@@ -28,18 +21,19 @@ export default {
 
         if (context.disabled) {
             stateStyling = 'bg-white opacity-50 cursor-not-allowed';
-        } else if (props.readOnly) {
-            stateStyling = 'bg-blue-200 border-transparent cursor-not-allowed';
-        } else if (props.variant === 'filled') {
-            successStyling = 'border-green-800 text-gray-500';
-        } else if (props.invalid) {
+        }
+
+        if (props.invalid) {
             stateStyling = 'border-red-800 bg-red-100';
             errorStyling = 'text-gray-500';
         }
 
-        const focusStyling = 'focus:ring-2 focus:ring-blue-800 focus:ring-offset-0';
+        const focusStyling = `focus:border-blue-800 focus:ring-4 focus:ring-blue-800 focus:ring-offset-0 ${props.invalid ? 'focus:border-blue-800 focus:text-black' : ''}`;
 
         const hoverStyling = !context.disabled ? 'hover:bg-blue-200' : '';
+
+        const hoverFocusStyling = `focus:bg-white ${hoverStyling}`;
+
         return {
             class: [
                 'relative',
@@ -52,10 +46,9 @@ export default {
                 sizeStyling,
                 stateStyling,
                 focusStyling,
-                successStyling,
                 'w-full',
-                !context.disabled && hoverStyling,
                 errorStyling,
+                hoverFocusStyling,
             ],
         };
     },
