90d65d35b9466b96b9a044cd72429f12fbc838c3
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-08-12 22:56:58 +0200

    Deletes style.css
    RISDEV-4536

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,6 @@
 import { setup } from "@storybook/vue3";
 import PrimeVue from "primevue/config";
 import PresetCollection from "../src/components/PresetCollection";
-import "../src/style.css";
 import '../src/global.css';
 
 setup((app) => {
