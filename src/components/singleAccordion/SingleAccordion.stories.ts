88617b609a6a02e00ff1a15a4507b06a21d3359f
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2024-09-16 15:47:01 +0200

    Add story

---
+++ src/components/singleAccordion/SingleAccordion.stories.ts
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from "@storybook/vue3";
+import SingleAccordion from ".";
+import { html } from "@/lib/tags.ts";
+import { ref } from "vue";
+
+const meta: Meta<typeof SingleAccordion> = {
+  component: SingleAccordion,
+
+  tags: ["autodocs"],
+
+  args: {
+    headerCollapsed: "Click to expand",
+    headerExpanded: "Click to collapse",
+    default:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+
+  argTypes: {
+    headerCollapsed: { control: "text" },
+    headerExpanded: { control: "text" },
+    default: { control: "text" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { SingleAccordion },
+    setup() {
+      const active = ref(true);
+      return { args, active };
+    },
+    template: html`<SingleAccordion v-bind="args" v-model="active"
+      >{{args.default}}</SingleAccordion
+    >`,
+  }),
+};
