c0a5c15f88ed4b77df774b67d06ea86720c0e255
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-09-30 18:20:13 +0200

    feat(textarea): adjust padding and apply consistent styling (#27)

--- src/primevue/textarea/textarea.ts
+++ src/primevue/textarea/textarea.ts
@@ -5,7 +5,7 @@ import "./textarea.css";
 const textarea: TextareaPassThroughOptions = {
   root: ({ props }) => {
     // Base
-    const base = tw`ris-body2-regular h-48 border-2 border-blue-800 bg-white px-16 py-10 outline-4 -outline-offset-4 outline-blue-800 placeholder:text-gray-900 read-only:cursor-not-allowed read-only:border-blue-300 read-only:bg-blue-300 hover:outline focus:outline disabled:border-blue-500 disabled:bg-white disabled:text-blue-500 disabled:outline-none aria-[invalid]:border-red-800 aria-[invalid]:bg-red-200 aria-[invalid]:outline-red-800 aria-[invalid]:disabled:outline-none`;
+    const base = tw`ris-body1-regular min-h-56 border-2 border-blue-800 bg-white px-14 pb-12 pt-8 outline-4 -outline-offset-4 outline-blue-800 placeholder:text-gray-900 read-only:cursor-not-allowed read-only:border-blue-300 read-only:bg-blue-300 hover:outline focus:outline disabled:border-blue-500 disabled:bg-white disabled:text-blue-500 disabled:outline-none aria-[invalid]:border-red-800 aria-[invalid]:bg-red-200 aria-[invalid]:outline-red-800 aria-[invalid]:disabled:outline-none`;
 
     // Integration for primevue/fluid
     const fluid = tw`w-full`;
