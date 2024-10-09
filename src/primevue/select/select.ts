25767747f843ac9fe74d2ea7b0c6c62a5ff04255
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-10-09 23:04:50 +0200

    refactor(select): simplify class bindings, remove redundant sizing, and improve fluid handling
    RISDEV-4987

--- src/primevue/select/select.ts
+++ src/primevue/select/select.ts
@@ -2,13 +2,14 @@ import { SelectPassThroughOptions } from "primevue/select";
 import { tw } from "@/lib/tags.ts";
 import "./select.css";
 
-export const base = tw`ris-body2-regular h-full min-h-48 w-full border-2 border-blue-800 bg-white px-16 py-4 outline-4 -outline-offset-4 outline-blue-800 placeholder:text-gray-600 hover:outline focus:outline disabled:border-blue-500 disabled:bg-white disabled:text-blue-500 disabled:outline-none [&+label]:my-4`;
+export const base = tw`ris-body2-regular min-h-48 border-2 border-blue-800 bg-white px-16 py-4 outline-4 -outline-offset-4 outline-blue-800 placeholder:text-gray-600 hover:outline focus:outline disabled:border-blue-500 disabled:bg-white disabled:text-blue-500 disabled:outline-none [&+label]:my-4`;
 
 const select: SelectPassThroughOptions = {
-  root: ({}) => {
+  root: ({ props }) => {
     return {
       class: {
         [base]: true,
+        "w-full": !!props.fluid,
         "flex items-center justify-between": true,
       },
       tabindex: 0,
@@ -27,19 +28,13 @@ const select: SelectPassThroughOptions = {
     };
   },
   list: () => {
-    const listStyle = tw`shadow-md`;
     return {
-      class: {
-        [listStyle]: true,
-      },
+      class: tw`shadow-md`,
     };
   },
-  option: ({}) => {
-    const optionStyle = tw`ris-body2-regular relative h-full min-h-48 w-full bg-white px-24 py-16 after:absolute after:bottom-0 after:left-8 after:right-8 after:border-b after:border-gray-300 after:content-[''] last:after:border-b-0 hover:bg-gray-100`;
+  option: () => {
     return {
-      class: {
-        [optionStyle]: true,
-      },
+      class: tw`ris-body2-regular relative h-full min-h-48 w-full bg-white px-24 py-16 after:absolute after:bottom-0 after:left-8 after:right-8 after:border-b after:border-gray-300 after:content-[''] last:after:border-b-0 hover:bg-gray-100`,
       tabindex: 0,
     };
   },
