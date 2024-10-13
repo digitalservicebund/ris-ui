9b0980ca845bb915b294011c21f29763faf954bd
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-10-14 01:24:18 +0200

    feat(select): apply states
    RISDEV-4987

--- src/primevue/select/select.ts
+++ src/primevue/select/select.ts
@@ -2,43 +2,41 @@ import { SelectPassThroughOptions } from "primevue/select";
 import { tw } from "@/lib/tags.ts";
 import "./select.css";
 
-const base = tw`ris-body2-regular min-h-48 border-2 bg-white px-16 py-4 placeholder:text-gray-600 hover:outline focus:outline aria-[invalid]:border-red-800 aria-[invalid]:bg-red-200 aria-[invalid]:outline-red-800 [&+label]:my-4`;
+const outlineStyles = tw`border-blue-800 outline-4 -outline-offset-4 outline-blue-800`;
+
+const stateStyles = tw`hover:outline has-[span:focus]:outline has-[span[aria-expanded=true]]:outline`;
+
+const invalidStyles = tw`border-red-800 bg-red-200 outline-red-800`;
+
+const base = tw`ris-body2-regular flex min-h-48 items-center justify-between border-2 px-16 py-4 placeholder:text-gray-600 [&+label]:ml-8`;
 
 const select: SelectPassThroughOptions = {
   root: ({ props }) => {
+    console.log(props);
     return {
       class: {
         [base]: true,
         "w-full": !!props.fluid,
-        "flex items-center justify-between": true,
-        "border-blue-500 bg-white text-blue-500 outline-none": props.disabled,
-        "outline-4 -outline-offset-4 outline-blue-800 border-blue-800":
-          !props.disabled,
+        [outlineStyles]: !props.disabled,
+        [stateStyles]: !props.disabled,
+        "border-blue-500 text-blue-500 outline-none cursor-not-allowed":
+          props.disabled,
+        [invalidStyles]: props.invalid,
       },
-      tabindex: 0,
-      onKeydown: (event: KeyboardEvent) => {
-        if (event.key === "Enter" || event.key === " ") {
-          event.preventDefault();
-          const dropdown = event.currentTarget as HTMLElement;
-          dropdown.click();
-        }
-      },
+      "aria-invalid": props.invalid ? "true" : null,
     };
   },
   label: () => {
     return {
-      tabindex: -1,
+      class: tw`outline-none`,
     };
   },
-  list: () => {
-    return {
-      class: tw`shadow-md`,
-    };
+  listContainer: {
+    class: tw`overflow-auto shadow-md`,
   },
   option: () => {
     return {
-      class: tw`ris-body2-regular relative h-full min-h-48 w-full bg-white px-24 py-16 after:absolute after:bottom-0 after:left-8 after:right-8 after:border-b after:border-gray-300 after:content-[''] last:after:border-b-0 hover:bg-gray-100`,
-      tabindex: 0,
+      class: tw`ris-body2-regular relative h-full min-h-48 w-full bg-white px-24 py-16 after:absolute after:bottom-0 after:left-8 after:right-8 after:border-b after:border-gray-300 after:content-[''] last:after:border-b-0 hover:bg-gray-100 data-[p-focused=true]:bg-gray-100`,
     };
   },
 };
