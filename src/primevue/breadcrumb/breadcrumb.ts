8d0fe8a392cbb1211e4846330e4060ba980381fa
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-11-05 20:14:09 +0100

    fix(breadcrumb): simplify styling classes for items (#49)

--- src/primevue/breadcrumb/breadcrumb.ts
+++ src/primevue/breadcrumb/breadcrumb.ts
@@ -9,7 +9,7 @@ const breadcrumb: BreadcrumbPassThroughOptions = {
     class: tw`flex-no-wrap ris-label1-regular my-2 flex items-center break-all`,
   },
   itemLink: {
-    class: tw`ris-link1-regular inline-flex cursor-pointer items-center leading-tight no-underline outline-4 outline-offset-4 outline-blue-800 hover:underline focus:outline`,
+    class: tw`inline-flex cursor-pointer items-center text-blue-800 outline-4 outline-offset-4 outline-blue-800 hover:underline focus:outline`,
   },
   separator: {
     class: tw`mx-6 flex items-center text-gray-600`,
