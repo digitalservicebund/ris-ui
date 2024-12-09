625ad02353435425d6c5ef8a2a272175110991c3
Carl Gödecken <MasterCarl@users.noreply.github.com>
2024-12-09 09:40:38 +0100

    chore: add Vue to eslint config (#88)

--- eslint.config.js
+++ eslint.config.js
@@ -4,18 +4,27 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import { fileURLToPath, URL } from "node:url";
 import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
 
 export default [
   // Files
   includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
   { ignores: ["public/mockServiceWorker.js"] },
-  { files: ["**/*.{js,ts}"] },
+  { files: ["**/*.{js,ts,vue}"] },
 
   // Language options
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
 
   // Configs
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
   eslintConfigPrettier,
 ];
