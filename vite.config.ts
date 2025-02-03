18241d9ed9543dc35e0f9f0bf2edf0065ea3bb80
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-02-03 13:59:56 +0100

    build: restore Tailwind plugin build output

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
     lib: {
       entry: {
         "primevue/index": "src/primevue/index.ts",
+        "tailwind/index": "src/tailwind/index.ts",
         "components/index": "src/components/index.ts",
       },
     },
