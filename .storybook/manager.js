b556265b9e1c0c5100bd1799fa78461477b110e2
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-02-19 23:47:01 +0100

    revert: update Storybook branding

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,7 +2,7 @@ import { addons } from "@storybook/manager-api";
 import { create as createTheme } from "@storybook/theming/create";
 
 const theme = createTheme({
-  brandTitle: "RIS UI FEATURE BRANCH",
+  brandTitle: "RIS UI",
 });
 
 addons.setConfig({ theme });
