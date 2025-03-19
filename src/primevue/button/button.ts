127672dc49ba7836e5ef022b71e3067d5c23cba8
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-03-19 15:29:30 +0100

    feat(button): add danger severity and small size to button (#166)

--- src/primevue/button/button.ts
+++ src/primevue/button/button.ts
@@ -13,23 +13,28 @@ const button: ButtonPassThroughOptions = {
 
     const secondary = tw`border-2 border-blue-800 bg-white text-blue-800 hover:bg-gray-200 focus:bg-gray-200 active:border-white active:bg-white disabled:border-blue-500 disabled:text-blue-500 disabled:hover:bg-white`;
 
+    const danger = tw`border-2 border-red-800 bg-white text-red-800 hover:bg-gray-200 focus:bg-gray-200 focus:outline-red-800 active:border-white active:bg-white disabled:border-red-500 disabled:text-red-500 disabled:hover:bg-white`;
+
     // Text
     // This is the "Ghost" variant of the button
     const primaryText = tw`border-2 border-transparent bg-transparent text-blue-800 underline hover:border-gray-500 hover:bg-white focus:border-gray-500 active:border-white active:bg-white disabled:bg-transparent disabled:text-gray-500`;
 
     // Sizes
     const size = props.size ?? "normal";
+    let small = tw`ris-body2-bold h-40 py-4`;
     let normal = tw`ris-body2-bold h-48 py-4`;
     let large = tw`ris-body1-bold h-64 py-4`;
 
     // Icon only
     if (instance.hasIcon && !props.label) {
+      small = tw`${small} w-40 px-4`;
       normal = tw`${normal} w-48 px-4`;
       large = tw`${large} w-64 px-4`;
     }
 
     // Label only or label + icon
     else {
+      small = tw`${small} px-16`;
       normal = tw`${normal} px-16`;
       large = tw`${large} px-24`;
     }
@@ -37,10 +42,12 @@ const button: ButtonPassThroughOptions = {
     return {
       class: {
         [base]: true,
+        [small]: size === "small",
         [normal]: size === "normal",
         [large]: size === "large",
         [primary]: !props.text && severity === "primary",
         [secondary]: !props.text && severity === "secondary",
+        [danger]: !props.text && severity === "danger",
         [primaryText]: props.text && severity === "primary",
       },
     };
