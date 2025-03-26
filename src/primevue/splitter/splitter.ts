b3052b4a6e405f8da9a266e5a51454d3c421f418
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-03-26 01:18:10 +0100

    fix: adjusts splitter focus height to 95%
    RISDEV-6942

--- src/primevue/splitter/splitter.ts
+++ src/primevue/splitter/splitter.ts
@@ -9,7 +9,11 @@ const splitter: SplitterPassThroughOptions = {
     class: tw`flex w-4 shrink-0 items-center justify-center bg-gray-400 transition-all duration-200 ${props.layout === "horizontal" ? " h-full cursor-col-resize" : "h-4 w-full cursor-row-resize"} `,
   }),
   gutterHandle: ({ props }) => ({
-    class: tw`active:outline-hidden, z-20 rounded-full transition-all duration-200 focus:outline focus:outline-2 focus:outline-blue-800 ${props.layout === "horizontal" ? "h-full w-4" : "w-full h-4"} `,
+    class: tw`z-20 rounded-full transition-all duration-200 focus:outline focus:outline-2 focus:outline-blue-800 active:outline-hidden ${
+      props.layout === "horizontal"
+        ? "h-full w-4 focus:h-[95%]"
+        : "w-full h-4 focus:w-[95%]"
+    } `,
   }),
 };
 
