bd5f369bca0df5e368dc2ba4664e1d0173aa498b
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-04-01 09:21:20 +0200

    fix(datatable): refine datatable styles
    For now, the datatable only supports very basic styling for headers and
    columns.

--- src/primevue/dataTable/dataTable.ts
+++ src/primevue/dataTable/dataTable.ts
@@ -2,64 +2,21 @@ import { DataTablePassThroughOptions } from "primevue/datatable";
 import { tw } from "@/lib/tags";
 
 const dataTable: DataTablePassThroughOptions = {
-  root: ({ props }) => ({
-    class: {
-      [tw`relative`]: true,
-      [tw`flex h-full flex-col`]:
-        props.scrollable && props.scrollHeight === "flex",
-    },
-  }),
-
-  tableContainer: ({ props }) => ({
-    class: {
-      [tw`relative`]: props.scrollable,
-      [tw`flex h-full grow flex-col`]:
-        props.scrollable && props.scrollHeight === "flex",
-    },
-  }),
-
   table: {
-    class: tw`w-full border-collapse`,
+    class: tw`w-full text-left`,
   },
 
-  thead: ({ props }) => ({
-    class: {
-      [tw`sticky top-0 z-40 bg-white`]: props.scrollable,
-    },
-  }),
-
-  bodyRow: {
-    class: tw`border-t border-blue-300 hover:bg-blue-200`,
-  },
-
-  rowExpansionCell: {
-    class: tw`p-0`,
+  tbody: {
+    class: tw`ris-body2-regular divide-y divide-blue-300`,
   },
 
   column: {
     headerCell: {
-      class: tw`ris-body2-bold py-8 pr-32 pl-8 text-center`,
+      class: tw`ris-body2-bold px-14 py-12`,
     },
 
-    columnHeaderContent: {
-      class: tw`flex items-center gap-2`,
-    },
-
-    bodyCell: ({
-      props,
-      context,
-    }: {
-      props: { expander?: boolean };
-      context: { type?: string; column?: { props?: { expander?: boolean } } };
-    }) => {
-      const isExpander =
-        props?.expander ||
-        context?.type === "expansion" ||
-        context?.column?.props?.expander;
-
-      return isExpander
-        ? { class: tw`w-6 py-8 pl-8 text-blue-800` }
-        : { class: tw`ris-body2-regular py-8 pr-32 pl-8 hover:bg-blue-200` };
+    bodyCell: {
+      class: tw`px-14 py-12`,
     },
   },
 };
