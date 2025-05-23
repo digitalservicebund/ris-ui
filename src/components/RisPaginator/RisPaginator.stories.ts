abe86053710b6031c01021501f86ab02b15fac9e
Xavier <xavier.tremel@digitalservice.bund.de>
2025-05-23 15:25:26 +0200

    feat(paginator): add wrapper (#229)

---
+++ src/components/RisPaginator/RisPaginator.stories.ts
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from "@storybook/vue3";
+import RisPaginator from "./RisPaginator.vue";
+import { html } from "@/lib/tags.ts";
+
+const meta: Meta<typeof RisPaginator> = {
+  component: RisPaginator,
+
+  tags: ["autodocs"],
+
+  args: {
+    first: 0,
+    rows: 10,
+    totalRecords: 100,
+  },
+
+  argTypes: {
+    first: {
+      description: "Index of the first record to display",
+      control: { type: "number" },
+    },
+    rows: {
+      description: "Number of rows to display per page",
+      control: { type: "number" },
+    },
+    totalRecords: {
+      description: "Total number of records available",
+      control: { type: "number" },
+    },
+    prevButtonLabel: {
+      description: "Label for the previous button",
+      control: { type: "text" },
+    },
+    nextButtonLabel: {
+      description: "Label for the next button",
+      control: { type: "text" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { RisPaginator },
+    setup() {
+      return { args };
+    },
+    template: html` <RisPaginator v-bind="args" /> `,
+  }),
+};
