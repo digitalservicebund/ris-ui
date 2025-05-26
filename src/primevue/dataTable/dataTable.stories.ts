2856c3ecd7641fc65bfe0ad15416003e43c80ebb
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-05-26 15:18:05 +0200

    feat(dataTable): add more states, change styles (#230)
    * feat(dataTable): add more states, change styles
    
    * feat(dataTable): add workaround for selected row styling
    
    * fix(dataTable): overflow behaviour
    
    * fix(dataTable): use border instead of box shadow
    
    * fix(dataTable): vertically align selection checkboxes in selectable tablecolumn
    
    * fix(dataTable): small ui fixes
    
    This adds:
    
    hover and selected styles for rows
    empty and loading state styles
    paddings, font and border updates

--- src/primevue/dataTable/dataTable.stories.ts
+++ src/primevue/dataTable/dataTable.stories.ts
@@ -2,6 +2,7 @@ import { html } from "@/lib/tags.ts";
 import { Meta, StoryObj } from "@storybook/vue3";
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
+import { ref } from "vue";
 
 const meta: Meta<typeof DataTable> = {
   // @ts-expect-error Component type broken
@@ -101,11 +102,88 @@ export const Default: Story = {
     },
     template: html`
       <div class="card">
+        <DataTable v-bind="args" tableStyle="min-width: 50rem">
+          <Column field="code" header="Code"></Column>
+          <Column field="name" header="Name"></Column>
+          <Column field="category" header="Category"></Column>
+          <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+      </div>
+    `,
+  }),
+};
+
+export const Selectable: Story = {
+  args: {
+    value: sampleProducts,
+  },
+  render: (args) => ({
+    components: { DataTable, Column },
+    setup() {
+      const selectedProducts = ref([]);
+      return { args, selectedProducts };
+    },
+    template: html`
+      <div class="card">
+        <DataTable
+          v-bind="args"
+          v-model:selection="selectedProducts"
+          tableStyle="min-width: 50rem"
+          dataKey="id"
+          selectionMode="multiple"
+          highlightOnSelect
+        >
+          <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+          <Column field="code" header="Code"></Column>
+          <Column field="name" header="Name"></Column>
+          <Column field="category" header="Category"></Column>
+          <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+      </div>
+    `,
+  }),
+};
+export const Loading: Story = {
+  args: {
+    value: [],
+  },
+  render: (args) => ({
+    components: { DataTable, Column },
+    setup() {
+      return { args };
+    },
+    template: html`
+      <div style="margin-top: -10px;">
+        <DataTable v-bind="args" loading>
+          <Column field="code" header="Code"></Column>
+          <Column field="name" header="Name"></Column>
+          <Column field="category" header="Category"></Column>
+          <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+      </div>
+    `,
+  }),
+};
+
+export const Empty: Story = {
+  args: {
+    value: [],
+  },
+  render: (args) => ({
+    components: { DataTable, Column },
+    setup() {
+      return { args };
+    },
+    template: html`
+      <div class="card">
         <DataTable v-bind="args">
           <Column field="code" header="Code"></Column>
           <Column field="name" header="Name"></Column>
           <Column field="category" header="Category"></Column>
           <Column field="quantity" header="Quantity"></Column>
+          <template #empty>
+            <div>Keine Daten gefunden.</div>
+          </template>
         </DataTable>
       </div>
     `,
--- src/primevue/dataTable/dataTable.ts
+++ src/primevue/dataTable/dataTable.ts
@@ -1,23 +1,57 @@
-import { DataTablePassThroughOptions } from "primevue/datatable";
+import {
+  DataTablePassThroughOptions,
+  DataTableContext,
+} from "primevue/datatable";
 import { tw } from "@/lib/tags";
 
 const dataTable: DataTablePassThroughOptions = {
+  tableContainer: {
+    class: tw`relative overflow-visible`,
+  },
   table: {
-    class: tw`w-full text-left`,
+    class: tw`w-full border-none text-left`,
+  },
+
+  thead: {
+    style: {
+      borderBottom: "1px solid var(--color-blue-300)",
+    },
   },
 
   tbody: {
-    class: tw`ris-body2-regular divide-y divide-blue-300`,
+    class: tw`divide-y divide-blue-300`,
   },
 
+  bodyRow: {
+    class: tw`hover:bg-blue-100`,
+    // The selected row is registered correctly, but this styling is not attached correctly:
+    // https://github.com/primefaces/primevue/issues/7759
+    style: ({ context }: { context: DataTableContext }) => ({
+      backgroundColor: context.selected ? "var(--color-blue-300)" : undefined,
+    }),
+  },
+
+  // height for td works like min-height: Table cells will grow when the content does not fit.
   column: {
     headerCell: {
-      class: tw`ris-body2-bold px-14 py-12`,
+      class: tw`ris-label2-bold h-56 px-14 py-12`,
     },
 
     bodyCell: {
-      class: tw`px-14 py-12`,
+      class: tw`ris-body1-regular h-56 px-16 py-12 has-[input[type=checkbox]]:flex has-[input[type=checkbox]]:items-center`,
     },
+  },
+
+  mask: {
+    class: tw`text-primary-500 justify-content absolute inset-0 z-10 mt-56 flex justify-center py-96`,
+  },
+
+  loadingIcon: {
+    class: tw`inline-block h-24 w-24 animate-spin border-solid border-r-transparent text-blue-900 motion-reduce:animate-[spin_1.5s_linear_infinite]`,
+  },
+
+  emptyMessageCell: {
+    class: tw`py-96 text-center text-gray-900`,
   },
 };
 
