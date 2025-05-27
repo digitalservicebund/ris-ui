e791cc4eed24999fb73952b24fed6611648ac213
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-05-27 16:55:28 +0200

    feat(tabs): add default styles to config

--- src/primevue/tabs/tabs.ts
+++ src/primevue/tabs/tabs.ts
@@ -6,7 +6,7 @@ import type { TabListPassThroughOptions } from "primevue/tablist";
 export const tab: TabPassThroughOptions = {
   root: ({ context }) => {
     const base = tw`ris-body2-bold h-64 border-b-4 border-b-transparent py-4 pr-24 pl-20 outline-0 -outline-offset-4 outline-blue-800 focus-visible:outline-4`;
-    const active = tw`z-10 bg-[var(--p-tabs-tab-active-background,white)] text-black shadow-[-1px_-1px_0_0_var(--p-tabs-tab-border-color,var(--color-gray-600)),1px_-1px_0_0_var(--p-tabs-tab-border-color,var(--color-gray-600))]`;
+    const active = tw`z-10 bg-[var(--p-tabs-tab-active-background)] text-black shadow-[-1px_-1px_0_0_var(--p-tabs-tab-border-color),1px_-1px_0_0_var(--p-tabs-tab-border-color)]`;
     const inactive = tw`cursor-pointer text-blue-800 hover:border-b-blue-800`;
     return {
       class: {
@@ -24,6 +24,6 @@ export const tabPanel: TabPanelPassThroughOptions = {
 };
 export const tabList: TabListPassThroughOptions = {
   tabList: {
-    class: tw`relative flex before:absolute before:bottom-0 before:left-[50%] before:h-px before:w-[var(--tab-list-separator-width,100%)] before:-translate-x-1/2 before:bg-[var(--p-tabs-tablist-border-color,var(--color-gray-600))]`,
+    class: tw`relative flex before:absolute before:bottom-0 before:left-[50%] before:h-px before:w-[var(--tab-list-separator-width,100%)] before:-translate-x-1/2 before:bg-[var(--p-tabs-tablist-border-color)]`,
   },
 };
