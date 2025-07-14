03b51669cee67af801646e7c44850c7525788165
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-07-14 08:57:50 +0200

    style: format code with prettier 3.6.2 to fix check-style

--- src/primevue/splitter/splitter.ts
+++ src/primevue/splitter/splitter.ts
@@ -11,8 +11,7 @@ const splitter: SplitterPassThroughOptions = {
 
   gutter: ({ props }) => ({
     class: {
-      [tw`flex shrink-0 items-center justify-center bg-gray-400 hover:bg-blue-700`]:
-        true,
+      [tw`flex shrink-0 items-center justify-center bg-gray-400 hover:bg-blue-700`]: true,
       [tw`h-full w-[3px] cursor-col-resize`]: props.layout === "horizontal",
       [tw`h-[3px] w-full cursor-row-resize`]: props.layout === "vertical",
     },
@@ -20,8 +19,7 @@ const splitter: SplitterPassThroughOptions = {
 
   gutterHandle: ({ props }) => ({
     class: {
-      [tw`z-20 rounded bg-transparent outline-offset-2 outline-blue-800 focus:outline-2`]:
-        true,
+      [tw`z-20 rounded bg-transparent outline-offset-2 outline-blue-800 focus:outline-2`]: true,
       [tw`h-11/12 w-[3px] cursor-col-resize`]: props.layout === "horizontal",
       [tw`h-[3px] w-11/12 cursor-row-resize`]: props.layout === "vertical",
     },
