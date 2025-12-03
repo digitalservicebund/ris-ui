76e7822eb3499805dce8bcff5a1015c8be1a40d5
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-12-03 12:29:15 +0100

    fix(RisExpandableText): use focus-visible for displaying outline
    RISDEV-9457

--- src/components/RisExpandableText/RisExpandableText.vue
+++ src/components/RisExpandableText/RisExpandableText.vue
@@ -50,7 +50,7 @@ watchEffect(() => {
 
     <button
       v-if="canExpand"
-      class="ris-link1-regular cursor-pointer focus:outline-4 focus:outline-offset-4 focus:outline-blue-800 active:outline-hidden"
+      class="ris-link1-regular cursor-pointer focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800 active:outline-hidden"
       :aria-controls="textId"
       :aria-expanded="expanded"
       @click="expanded = !expanded"
