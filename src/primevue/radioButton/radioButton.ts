e395655f86baa8d97a7571a18fa6e346fbb6875c
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-12-03 12:30:25 +0100

    fix(radioButton): use focus-visible for displaying outline
    RISDEV-9457

--- src/primevue/radioButton/radioButton.ts
+++ src/primevue/radioButton/radioButton.ts
@@ -7,7 +7,7 @@ const radioButton: RadioButtonPassThroughOptions = {
   },
 
   input: {
-    class: tw`peer h-full w-full cursor-pointer appearance-none rounded-full border-2 border-blue-800 bg-white hover:outline hover:outline-4 hover:-outline-offset-4 hover:outline-blue-800 focus:outline focus:outline-4 focus:-outline-offset-4 focus:outline-blue-800 active:outline-hidden disabled:cursor-not-allowed disabled:border-gray-600 aria-[invalid]:border-red-800 aria-[invalid]:outline-red-800 aria-[invalid]:active:outline-hidden aria-[invalid]:disabled:outline-hidden`,
+    class: tw`peer h-full w-full cursor-pointer appearance-none rounded-full border-2 border-blue-800 bg-white hover:outline hover:outline-4 hover:-outline-offset-4 hover:outline-blue-800 focus-visible:outline focus-visible:outline-4 focus-visible:-outline-offset-4 focus-visible:outline-blue-800 active:outline-hidden disabled:cursor-not-allowed disabled:border-gray-600 aria-[invalid]:border-red-800 aria-[invalid]:outline-red-800 aria-[invalid]:active:outline-hidden aria-[invalid]:disabled:outline-hidden`,
   },
 
   box: {
