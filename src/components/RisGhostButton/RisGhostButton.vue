1766b5eebaeb88e71740ba0b62eada4b83d9788a
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-12-08 13:51:18 +0100

    fix(RisAutoComplete, RisAutoCompleteMultiple): dropdown icon should be blue

--- src/components/RisGhostButton/RisGhostButton.vue
+++ src/components/RisGhostButton/RisGhostButton.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <button
     :class="{
-      'cursor-pointer p-8 focus-visible:bg-blue-800 focus-visible:text-white focus-visible:outline-none': true,
+      'cursor-pointer p-8 text-blue-800 focus-visible:bg-blue-800 focus-visible:text-white focus-visible:outline-none': true,
       'bg-blue-800 text-white': forceFocus,
       'hover:bg-blue-100 hover:text-blue-800': !forceFocus,
     }"
