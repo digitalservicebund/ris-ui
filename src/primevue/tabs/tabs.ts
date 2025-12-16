cf9bf374da1fce63f21a714f57602b3cdbc16bbe
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2025-12-16 16:02:03 +0100

    fix(tabs): remove z-index, fixes stacking issues with overlays (#409)
    RISDEV-10198

--- src/primevue/tabs/tabs.ts
+++ src/primevue/tabs/tabs.ts
@@ -6,7 +6,7 @@ import type { TabListPassThroughOptions } from "primevue/tablist";
 export const tab: TabPassThroughOptions = {
   root: ({ context }) => {
     const base = tw`ris-body2-bold h-64 border-b-4 border-b-transparent py-4 pr-24 pl-20 outline-0 -outline-offset-4 outline-blue-800 focus-visible:outline-4`;
-    const active = tw`z-10 bg-[var(--p-tabs-tab-active-background)] text-black shadow-[-1px_-1px_0_0_var(--p-tabs-tab-border-color),1px_-1px_0_0_var(--p-tabs-tab-border-color)]`;
+    const active = tw`bg-(--p-tabs-tab-active-background) text-black shadow-[-1px_-1px_0_0_var(--p-tabs-tab-border-color),1px_-1px_0_0_var(--p-tabs-tab-border-color)]`;
     const inactive = tw`cursor-pointer text-blue-800 hover:border-b-blue-800`;
     return {
       class: {
@@ -19,11 +19,11 @@ export const tab: TabPassThroughOptions = {
 };
 export const tabPanel: TabPanelPassThroughOptions = {
   root: {
-    class: tw`min-h-96 bg-[var(--p-tabs-tabpanel-background)] py-24 outline-blue-800 focus-visible:outline-solid`,
+    class: tw`min-h-96 bg-(--p-tabs-tabpanel-background) py-24 outline-blue-800 focus-visible:outline-solid`,
   },
 };
 export const tabList: TabListPassThroughOptions = {
   tabList: {
-    class: tw`relative flex before:absolute before:bottom-0 before:left-[50%] before:h-px before:w-[var(--tab-list-separator-width,100%)] before:-translate-x-1/2 before:bg-[var(--p-tabs-tablist-border-color)]`,
+    class: tw`relative flex before:absolute before:bottom-0 before:left-[50%] before:h-px before:w-(--tab-list-separator-width,100%) before:-translate-x-1/2 before:bg-(--p-tabs-tablist-border-color)`,
   },
 };
